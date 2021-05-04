fe3895fb109e00f748d2dc8bc5ab248429a484b1
2ndyrGroupA <2ndyrGroupA@pn.local>
2021-05-04 16:46:58 +0800

    can not update and retrieve

--- dist/index.php
+++ dist/index.php
@@ -1,8 +1,10 @@
 <?php
-require_once('task.php');
-$myTask=new tasks;
-
-
+session_start();
+require_once 'task.php';
+$myTask = new tasks;
+$myTask->addTask();
+$myTask->deleteTask();
+$myTask->updateTask();
 
 ?>
 
@@ -25,12 +27,17 @@ $myTask=new tasks;
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"crossorigin="anonymous"
         ></script>
         <link href="/your-path-to-fontawesome/css/fontawesome.css"rel="stylesheet"/>
-        <link href="/your-path-to-fontawesome/css/brands.css"rel="stylesheet"/>
-        <link href="/your-path-to-fontawesome/css/solid.css" rel="stylesheet" />
+
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <style>
+    ion-icon {
+  font-size: 20px;
+  }
+    </style>
+    
     </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
@@ -95,13 +102,13 @@ $myTask=new tasks;
                     <div class="sb-sidenav-menu">
                         <div class="nav">
                             <div class="sb-sidenav-menu-heading">Core</div>
-                            <a class="nav-link" href="index.html">
+                            <a id="taskNav"class="nav-link" href="#">
                                 <div class="sb-nav-link-icon">
                                     <i class="fas fa-list -alt"></i>
                                 </div>
                                 Task
                             </a>
-                            <a class="nav-link" href="index.html">
+                            <a  id="trash" class="nav-link" href="#">
                                 <div class="sb-nav-link-icon">
                                     <i class="fas fa-trash-alt"></i>
                                 </div>
@@ -112,39 +119,47 @@ $myTask=new tasks;
                 </nav>
             </div>
             <div id="layoutSidenav_content">
-                <main>
-                    <div class="container-fluid">
-                        <h1 class="mt-4">Your To-do List</h1>
+          
+              <div class="contanier">
+                    <div class="container-fluid ">
+                        <h1 class="indicator mt-4">Your To-Do List</h1>
                         <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item active">Dashboard</li>
+                           
                         </ol>
-                        <div class="row">
-                            <?php $myTask->displayData(); ?>
-                            
-
-                            
+                        <div class="row ">
+                            <?php $myTask->displayData();?>
+                            <?php  $myTask->displayTrash();?>
+                           
                         </div>
                     </div>
-                </main>
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid">
-                        <div
-                            class="d-flex align-items-center justify-content-between small"
-                        >
-                            <div class="text-muted">
-                                Copyright &copy; Your Website 2021
-                            </div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
+                    <button style=" position:absolute; margin-top:180px; margin-left:1000px" type="button" data-toggle="modal" data-target="#staticBackdrop" class="btn btn-info btn-sm rounded-circle shadow p-3 mb-5 bg-info rounded "><ion-icon size="large" name="add-outline"></ion-icon></button>
+                   
+                   <!-- modal for adding a task -->
+                    <div class="modal fade" id="staticBackdrop" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="staticBackdropLabel">Add Tasks here</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <form method="post">
+                        <div class="modal-body">
+                            <input type="text" name="task" class="form-control form-control-lg border-info"  placeholder="Enter Task here....">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                            <button type="submit" id="addTask" name="addTask"class="btn btn-primary">submit</button>
+                        </div>
+                        </form>
                         </div>
                     </div>
-                </footer>
+                    </div>
+                   
             </div>
         </div>
-       
+        <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
         <script
             src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
             crossorigin="anonymous"
@@ -153,8 +168,40 @@ $myTask=new tasks;
             src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
             crossorigin="anonymous"
         ></script>
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
         <script src="js/scripts.js"></script>
          <script>
+        $("#trash").on('click',()=>{
+            $('.indicator').text("Trash")
+            $('.undeletedTask').hide();
+            $('.deletedTask').show();
+
+        });
+
+        $("#taskNav").on('click',()=>{
+            $('.indicator').text("Your To-Do List")
+            $('.undeletedTask').show();
+            $('.deletedTask').hide();
+        });
+         $(".editBtn").on('click',()=>{
+
+         console.log($(this))
+
+         });
+         <?php   
+          if(isset($_POST['addTask'])&& $_POST['task'] !=""){
+         ?>
+            Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Your task has been added',
+            showConfirmButton: false,
+            timer: 1500
+            })
+      
+
+          <?php }?>
+
             $('.kebab').on('click',()=>{
                 console.log($(this).children);
                 middle = document.querySelector('.middle'),
@@ -164,7 +211,6 @@ $myTask=new tasks;
                 cross.classList.toggle('active');
                 dropdown.classList.toggle('active');
             })
-
 
         </script>
     </body>
--- dist/task.php
+++ dist/task.php
@@ -1,116 +1,140 @@
 <?php
-
 	class tasks
-	{
-		private $servername = "localhost";
-		private $username   = "root";
-		private $password   = "";
-		private $database   = "to_do_list";
-		public  $con;
+    {
+        private $server = "mysql:host=localhost;dbname=to_do_list";
+        private $user = "";
+        private $password = "";
+        private $options = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        );
 
+        protected $connection;
 
-		// Database Connection 
-		public function __construct()
-		{
-		    $this->con = new mysqli($this->servername, $this->username,$this->password,$this->database);
-		    if(mysqli_connect_error()) {
-			 trigger_error("Failed to connect to MySQL: " . mysqli_connect_error());
-		    }else{
-			return $this->con;
-		    }
-		}
+        //connections
+        public function openConnection()
+        {
+            try {
+               
+                $this->connection= new PDO(
+                    $this->server,
+                    $this->user,
+                    $this->password,
+                    $this->options
+                );
+               
+                return $this->connection;
+            } catch (PDOException $error) {
+                echo "Erro connection:" . $error->getMessage();
+            }
+        }
+        public function closeConnection()
+        {
+            $this->$connection= null;
+        }
 
-		// Fetch customer records for show listing
-		public function displayData()
-		{
-		    $query = "SELECT to_do FROM information";
-		    $result = $this->con->query($query);
-		if ($result->num_rows > 0) {
-		    $data = array();
-		    while ($row = $result->fetch_assoc()) {
-                   
-                   
-                   echo '<div class="col-xl-3 col-md-6">
-                   <div class="card bg-info text-white mb-4">
-                    
-                       <div class="card-body ml-4">
-                           <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                           '.$row["to_do"].'</div>
-                       <div
-                           class="card-footer d-flex align-items-center justify-content-between"
-                       >
-                          
-                       <div class="container-fluid float-right" style="margin-right:-40px;">
-                          
-                           <ul class="nav float-right"> 
-                             <div class="kebab">
-                               <figure></figure>
-                               <figure class="middle"></figure>
-                               <p class="cross">x</p>
-                               <figure></figure> 
-                               <ul class="m-dropdown">  
-                                 <li><a href="http://www.g.com">Art</a></li>
-                                 <li><a href="http://www.g.com">Coding</a></li>
-                                 <li><a href="http://www.g.com">Design</a></li>
-                                 <li><a href="http://www.g.com">Web Development</a></li>
-                               </ul>
-                             </div>
-                           </ul>
-                             </div>
-                       </div>
-                   </div>
-               </div>';
-		    }
-			 return $data;
-		    }else{
-			 echo "No found records";
-		    }
-		}
-
-		// Fetch single data for edit from customer table
-		public function displyaRecordById($id)
-		{
-		    $query = "SELECT * FROM customers WHERE id = '$id'";
-		    $result = $this->con->query($query);
-		if ($result->num_rows > 0) {
-			$row = $result->fetch_assoc();
-			return $row;
-		    }else{
-			echo "Record not found";
-		    }
-		}
-
-		// Update customer data into customer table
-		public function updateRecord($postData)
-		{
-		    $name = $this->con->real_escape_string($_POST['uname']);
-		    $email = $this->con->real_escape_string($_POST['uemail']);
-		    $username = $this->con->real_escape_string($_POST['upname']);
-		    $id = $this->con->real_escape_string($_POST['id']);
-		if (!empty($id) && !empty($postData)) {
-			$query = "UPDATE customers SET name = '$name', email = '$email', username = '$username' WHERE id = '$id'";
-			$sql = $this->con->query($query);
-			if ($sql==true) {
-			    header("Location:index.php?msg2=update");
-			}else{
-			    echo "Registration updated failed try again!";
+        public function displayData()
+        {
+            $connection = $this->openConnection();
+            $statement = $connection->prepare("SELECT * FROM tasks WHERE deletedAt is null");
+            $statement->execute();
+            
+            $taskCount = $statement->rowCount();
+			while ($row = $statement->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+					echo '<div class="col-xl-3 col-md-6 undeletedTask">
+					<div class="card bg-info text-white mb-4">
+					 
+						<div class="card-body ml-4">
+							
+						<p>	'.$row[1].'</p></div>
+						<divclass="card-footer d-flex align-items-center justify-content-between">   
+							<div class="container-fluid float-right" style="margin-right:-40px;">
+						   
+								<div class="row"> <input type="checkbox" class="form-check-input" id="exampleCheck1">
+									<small>Mark as done</small>
+									<form method="post">
+									
+										<input type="hidden" name="ID" value="'.$row[0].'">
+										<button  type="button"  class="btn btn-outline-secondary btn-sm ml-3 text-light"data-toggle="modal" data-target="#exampleModalCenter" ></ion-icon><ion-icon class="ml-1" name="create-outline"></ion-icon></button>
+										<button type="submit" class="btn btn-outline-secondary btn-sm text-light ml-2	" name="delete"><ion-icon   name="trash-outline"></button>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>';
+				 
 			}
-		    }
-			
-		}
+        }
 
 
-		// Delete customer data from customer table
-		public function deleteRecord($id)
-		{
-		    $query = "DELETE FROM customers WHERE id = '$id'";
-		    $sql = $this->con->query($query);
-		if ($sql==true) {
-			header("Location:index.php?msg3=delete");
-		}else{
-			echo "Record does not delete try again";
-		    }
-		}
+		public function displayTrash()
+        {
+            $connection = $this->openConnection();
+            $statement = $connection->prepare("SELECT * FROM tasks WHERE deletedAt is not null");
+            $statement->execute();
+            
+            $taskCount = $statement->rowCount();
+			while ($row = $statement->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+					echo '<div class="col-xl-3 col-md-6 deletedTask" style="display:none">
+					<div class="card bg-info text-white mb-4">
+					 
+						<div class="card-body ml-4">
+							
+						<p>	'.$row[1].'</p></div>
+						<div
+							class="card-footer d-flex align-items-center justify-content-between">   
+						<div class="container-fluid float-right" style="margin-right:-40px;">
+							<div class="row"> 
+							<small>Retrieve</small>
+							<form method="post">
+								<input type="hidden" name="ID" value="'.$row[0].'">
+								<button type="submit" class=" ml-5 btn btn-outline-secondary btn-sm text-light" name="delete"><ion-icon   name="trash-outline"></button>
+						
+							</div>
+							  </div>
+						</div>
+					</div>
+				</div>';
+				 
+			}
+        }
 
+		public function addTask(){
+            if(isset($_POST['addTask']) && $_POST['task'] !=""){
+                $dateCreated = date('Y-m-d H:i:s');
+                 $task=$_POST['task'];
+                 $connection =$this->openConnection();
+                 $statement=$connection->prepare("INSERT INTO  tasks(todo,createdAt) VALUES (?,?)");
+                 $statement->execute([$task,$dateCreated
+				 ]);
+            }
+         }
+
+		 public function deleteTask(){
+            if(isset($_POST['delete'])){
+                $dateDeleted = date('Y-m-d H:i:s');
+                 $ID=$_POST['ID'];
+                 $connection =$this->openConnection();
+                 $statement=$connection->prepare("UPDATE   tasks SET deletedAt=? WHERE id=$ID");
+                 $statement->execute([$dateDeleted]);
+            }
+         }
+       
+		 public function updateTask(){
+            if(isset($_POST['edit'])){
+                 $dateUpdate = date('Y-m-d H:i:s');
+				 $newTask=$_POST['newTask'];
+                 $ID= $_POST['oldTask'];
+                 $connection =$this->openConnection();
+                 $statement=$connection->prepare("UPDATE   tasks SET updatedAt=?, todo=? WHERE todo='$ID'");
+                 $statement->execute([$dateUpdate,$newTask]);
+            }
+         }
 	}
+       
+        
 ?>
+
+
+
